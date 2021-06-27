9a2863f6631ca40fa953b2ec52864e0f36428dc5
attilaberencsi <attilaberencsi@gmail.com>
2021-06-27 12:31:48 +0000

    Project Generation

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
+    return AppComponent.extend("customer.fe.demo.zfoondandbev.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
